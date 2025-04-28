deb008d22d8d9fdd2509661bdf9ef8ddd4f25161
Prince Edouard <pedouard2@gmail.com>
2025-04-28 09:28:00 -0700

    rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://candid-meringue-f1c759.netlify.app"
+  });
