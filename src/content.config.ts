e407cd84df65ea29313c59e90302ff3cd90831e6
Prince Edouard <pedouard2@gmail.com>
2025-04-28 12:31:13 -0700

    added job and education collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,5 +17,41 @@ const blog = defineCollection({
       tags: z.array(z.string())
     })
 });
+
+
+export const workExperienceCollection = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/job" }),
+    schema: z.object({
+      title: z.string(), // Job title
+      company: z.string(), // Company name
+      location: z.string(), // City, State or Remote
+      startDate: z.date(), // When you started
+      endDate: z.date().optional(), // When you left (optional for current positions)
+      current: z.boolean().default(false), // If this is your current job
+      achievements: z.array(z.string()), // Notable accomplishments
+      technologies: z.array(z.string()).optional(), // Technologies/tools used
+    })
+  });
+
+
+export const educationCollection = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/education" }),
+    schema: z.object({
+      degree: z.string(),
+      institution: z.string(),
+      location: z.string(),
+      graduationDate: z.date(),
+      fieldOfStudy: z.string(),
+      gpa: z.string().optional(),
+      relevantCoursework: z.array(z.string()).optional(),
+    })
+  });
+  
+
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = {
+    'workExperience': workExperienceCollection,
+    'education': educationCollection,
+  };
+
