11283d421f4ae0340b7918c45069ef5652e1b8b1
Prince Edouard <pedouard2@gmail.com>
2026-01-02 20:32:34 -0800

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://pedouard2.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
